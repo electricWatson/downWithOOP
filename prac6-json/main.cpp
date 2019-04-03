226ba5c61daf75fda4ddd4622d7ca30e7d6f4b25
goatHooves <sw182@zips.uakron.edu>
2019-04-02 23:38:20 -0400

    initial commit after trying for hours to pass json data to the code with no success and no code not being able to pass any json to the given code. tried different filestreams with no more success, revert to initial files and approach diffrently."

---
+++ prac6-json/main.cpp
@@ -0,0 +1,19 @@
+
+#include "json.hpp"
+
+#include <iterator>
+#include <iostream>
+#include <string>
+
+int
+main()
+{
+  std::istreambuf_iterator<char> first(std::cin);
+  std::istreambuf_iterator<char> last;
+  std::string s(first, last);
+
+  json::Value* val = json::parse(s);
+
+  // std::cout << size(val) << ' ' << height(val) << '\n';
+  std::cout << *val << '\n';
+}
